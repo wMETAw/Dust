dfa9346013aabaf09da12a05a09a7012b05eaee0
meta <wgn8404@gmail.com>
2014-07-27 10:20:09 +0900

    スワイプで物体に力を加える

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -1,39 +1,7 @@
 #include "GameScene.h"
+#include "GB2ShapeCache-x.h"
 
-USING_NS_CC;
-
-
-// タッチ開始処理
-void GameScene::ccTouchesBegan(CCSet* touches, CCEvent* event)
-{
-    CCSetIterator it;
-    for (it = touches->begin(); it != touches->end(); ++it)
-    {
-        CCTouch* touch = (CCTouch*)(*it);
-        
-        if(!touch) {
-            break;
-        }
-        
-        // タイトルへ
-        CCScene* TitleScene = TitleScene::scene();
-        
-        // ページめくりでシーンきりかえ【引数】アニメーション時間、シーンのポインタ、逆再生フラグ
-        CCDirector::sharedDirector()->replaceScene(CCTransitionPageTurn::create(0.5f, TitleScene, 0));
-    }
-}
-
-// 背景の作成
-void GameScene::createBackground()
-{
-    // 画面サイズの取得
-    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
-    
-    // 背景
-    CCSprite* bg = CCSprite::create("bg.png");
-    bg->setPosition(CCPoint(winSize.width/2, winSize.height/2));
-    this->addChild(bg, kZOrderBackground, kTagBackground);
-}
+#define PTM_RATIO 400
 
 // シーンの作成
 // @return シーン
@@ -59,11 +27,197 @@ bool GameScene::init()
         return false;
     }
     
+    // シェイプデータの読み込み
+    gbox2d::GB2ShapeCache::sharedGB2ShapeCache()->addShapesWithFile("test3.plist");
+    
+    // 物理ワールドの設定
+    initPhysics();
+    
     // タップを有効にする
     setTouchEnabled(true);
     
     // 背景の表示
     createBackground();
     
+    // ボール作成
+    createBall();
+    
+    // 毎フレームの処理の開始
+    scheduleUpdate();
+    
     return true;
 }
+
+// 物理ワールド初期化
+void GameScene::initPhysics()
+{
+    // 物理ワールドを生成し、重力を設定
+    b2Vec2 gravity;
+    gravity.Set(0.0f, -2.0f);
+    world = new b2World(gravity);
+}
+
+
+// 物理ワールド更新
+void GameScene::update(float dt)
+{
+    int velocityIterations = 8;
+    int positionIterations = 1;
+    
+    world->Step(dt, velocityIterations, positionIterations);
+    
+    // 物理ワールドすべての剛体をループ
+    for (b2Body* b = world->GetBodyList(); b; b = b->GetNext())
+    {
+        if(b->GetUserData() != NULL) {
+            CCSprite* myActor = (CCSprite*)b->GetUserData();
+            
+            if(b->GetPosition().y < 0) {
+                world->DestroyBody(b);
+                this->removeChild(myActor);
+                continue;
+            }
+            
+            myActor->setPosition(CCPointMake(b->GetPosition().x * PTM_RATIO, b->GetPosition().y * PTM_RATIO));
+            myActor->setRotation(-1 * CC_RADIANS_TO_DEGREES(b->GetAngle()));
+        }
+    }
+}
+
+// タッチ開始処理
+void GameScene::ccTouchesBegan(CCSet* touches, CCEvent* event)
+{
+    CCDirector* pDirector = CCDirector::sharedDirector();
+    
+    CCSetIterator it;
+    for (it = touches->begin(); it != touches->end(); ++it)
+    {
+        CCTouch* touch = (CCTouch*)(*it);
+        
+        if(!touch) {
+            break;
+        }
+        
+        // 画面上でのタッチ位置を取得
+        CCPoint tPoint = pDirector->convertToGL(touch->getLocationInView());
+        
+        // 物理ワールドすべての剛体をループ
+        for (b2Body* b = world->GetBodyList(); b; b = b->GetNext())
+        {
+            if(b->GetUserData() != NULL) {
+                
+                CCSprite* myActor = (CCSprite*)b->GetUserData();
+                CCRect rect = myActor->boundingBox();
+                
+                // あたり判定
+                if (rect.containsPoint(tPoint)) {
+                    
+                    touchBody = b;
+                    touchStartPoint = tPoint;
+                    break;
+                }
+                
+            }
+        }
+        
+        // タイトルへ
+        //CCScene* TitleScene = TitleScene::scene();
+        
+        // ページめくりでシーンきりかえ【引数】アニメーション時間、シーンのポインタ、逆再生フラグ
+        //CCDirector::sharedDirector()->replaceScene(CCTransitionPageTurn::create(0.5f, TitleScene, 0));
+        
+        //createBall();
+    }
+}
+
+// タッチ終了
+void GameScene::ccTouchesEnded(CCSet* touches, CCEvent* event)
+{
+    CCDirector* pDirector = CCDirector::sharedDirector();
+    
+    CCSetIterator it;
+    for (it = touches->begin(); it != touches->end(); ++it)
+    {
+        CCTouch* touch = (CCTouch*)(*it);
+        
+        if(!touch){
+            break;
+        }
+        
+        // 画面上でのタッチ位置を取得
+        CCPoint touchEndPoint = pDirector->convertToGL(touch->getLocationInView());
+        
+        // タッチした物体があれば力を加える
+        if (touchBody != NULL) {
+
+            float x = (touchEndPoint.x - touchStartPoint.x)*0.1;
+            float y = (touchEndPoint.y - touchStartPoint.y)*0.1;
+            touchBody->ApplyForce(b2Vec2(x, y), touchBody->GetWorldCenter());
+            touchBody = NULL;
+        }
+        
+    }
+    
+}
+
+
+// 背景の作成
+void GameScene::createBackground()
+{
+    // 画面サイズの取得
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    
+    // 背景
+    CCSprite* bg = CCSprite::create("bg.png");
+    bg->setPosition(CCPoint(winSize.width/2, winSize.height/2));
+    this->addChild(bg, kZOrderBackground, kTagBackground);
+    
+    // 背景の物理構造
+    b2BodyDef bgBodyDef;
+    bgBodyDef.type = b2_staticBody;
+    bgBodyDef.position.Set(bg->getPositionX() / PTM_RATIO, bg->getPositionY() / PTM_RATIO);
+    bgBodyDef.userData = bg;
+    bgBody = world->CreateBody(&bgBodyDef);
+    
+    gbox2d::GB2ShapeCache* sc = gbox2d::GB2ShapeCache::sharedGB2ShapeCache();
+    sc->addFixturesToBody(bgBody, "bg");
+    bg->setAnchorPoint(sc->anchorPointForShape("bg"));
+}
+
+// ボールの生成
+void GameScene::createBall()
+{
+    // ウィンドウサイズの取得
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    
+    // ボールの作成
+    CCPoint ballPoint = ccp(winSize.width * 0.25, winSize.height * 0.55);
+    CCSprite* ball = CCSprite::create("ball.png");
+    ball->setPosition(ballPoint);
+    this->addChild(ball, kZOrderBall);
+    
+    // ボールの物理構造
+    b2BodyDef ballBodyDef;
+    ballBodyDef.type = b2_dynamicBody;
+    ballBodyDef.position.Set(ballPoint.x / PTM_RATIO, ballPoint.y / PTM_RATIO);
+    ballBodyDef.userData = ball;
+    b2Body* body = world->CreateBody(&ballBodyDef);
+    
+    gbox2d::GB2ShapeCache* sc = gbox2d::GB2ShapeCache::sharedGB2ShapeCache();
+    sc->addFixturesToBody(body, "ball");
+    ball->setAnchorPoint(sc->anchorPointForShape("ball"));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -4,6 +4,10 @@
 #include "cocos2d.h"
 #include "TitleScene.h"
 #include "PlaySE.h"
+#include "Box2D/Box2D.h"
+
+
+USING_NS_CC;
 
 class GameScene : public cocos2d::CCLayer
 {
@@ -13,6 +17,10 @@ public:
     
     // cocosで使われる.create()関数が定義されている
     CREATE_FUNC(GameScene);
+    
+    // 物理ワールド更新
+    void update(float dt);
+    
     
 protected:
     
@@ -24,13 +32,28 @@ protected:
     // Zオーダー（表示順序）
     enum kZOder {
         kZOrderBackground = 1, // 背景
+        kZOrderBall,
     };
+    
+    // 物理ワールドの作成
+    b2World* world;
+    void initPhysics();
+    b2Body* bgBody;
     
     // 背景の作成
     void createBackground();
     
+    // ボールの作成
+    void createBall();
+    
     // タッチ開始の処理
     virtual void ccTouchesBegan(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
+    
+    // タッチ終了の処理
+    virtual void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
+    
+    CCPoint touchStartPoint;
+    b2Body* touchBody;
     
 };
 
